dd7ade7e5d29d0a648e05c54b2c32d58c414ef75
aristoo-11 <mohamed.gherabli@etu.univ-batna2.dz>
2024-07-19 03:23:21 +0100

    fix error,build for input,(phone,name,time,...)

--- lib/features/Screen/App/ReservationScreen.dart
+++ lib/features/Screen/App/ReservationScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:medi_dos_app/core/Theme/AppColor.dart';
+import 'package:medi_dos_app/core/Widgets/BuildReservation.dart';
 import 'package:medi_dos_app/core/Widgets/CustomButton.dart';
 import 'package:medi_dos_app/core/Widgets/CustomRangedate.dart';
 import 'package:medi_dos_app/core/Widgets/CustomReserve.dart';
@@ -15,52 +16,69 @@ class _ReservationPState extends State<ReservationP> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 1,
-        ),
-        body: SizedBox(
-          height: double.infinity,
-          child: Stack(children: [
-            const Image(
-              image: AssetImage(
-                'assets/raport/imgc.png',
+      appBar: AppBar(
+        elevation: 1,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          height: 900,
+          child: Stack(
+            children: [
+              const Image(
+                image: AssetImage(
+                  'assets/raport/imgc.png',
+                ),
+                width: double.infinity,
+                height: 390,
               ),
-              width: double.infinity,
-              height: 390,
-            ),
-            const Positioned(
-              top: 300,
-              left: 43,
-              right: 42,
-              child: CustomReserve(
-                address: 'Batna.Kchida',
-                name: 'AL-Ihsaniyat',
-                phone: '033224214',
+              const Positioned(
+                top: 300,
+                left: 43,
+                right: 42,
+                child: CustomReserve(
+                  address: 'Batna.Kchida',
+                  name: 'AL-Ihsaniyat',
+                  phone: '033224214',
+                ),
               ),
-            ),
-            const Positioned(
+              const Positioned(
                 top: 450,
                 left: 43,
                 right: 42,
                 child: Text(
                   'Available Time',
                   style: TextStyle(
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                      color: AppColors.black),
-                )),
-            const Positioned(
-                top: 480, left: 43, right: 42, child: CustomDatePicker()),
-            Positioned(
-              top: 600,
-              left: 43,
-              right: 42,
-              child: CustomButton(
-                onpress: () {},
-                textB: 'Confirm',
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.black,
+                  ),
+                ),
               ),
-            )
-          ]),
-        ));
+              const Positioned(
+                top: 480,
+                left: 43,
+                right: 42,
+                child: CustomDatePicker(),
+              ),
+               Positioned(
+                top: 550,
+                left: 43,
+                right: 42,
+                child: BuildReservation(),
+              ),
+              Positioned(
+                top: 840, // Adjusted the position to avoid overlap
+                left: 43,
+                right: 42,
+                child: CustomButton(
+                  onpress: () {},
+                  textB: 'Confirm',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
