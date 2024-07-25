5a9cc67fdfb8852a4e7b42b2f9671d758779788d
aristoo-11 <mohamed.gherabli@etu.univ-batna2.dz>
2024-07-25 04:42:35 +0100

    hosting Api,baseurl,End point

--- lib/core/Services/Endpoint.dart
+++ lib/core/Services/Endpoint.dart
@@ -1,5 +1,5 @@
 class Endpoint {
-  static String BaseUrl = 'http://10.0.2.2:3000/';
+  static String BaseUrl = ' https://medidos-api-1.onrender.com ';
   static String Login = 'user/login';
   static String SignUp = 'user/signup';
   static String GetClinic = 'clinic/';
